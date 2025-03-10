ea4ef24010dbe1cb0a965856002354b1b799ffdf
miguelFM333 <miguelfmarchi@gmail.com>
2025-03-10 12:24:21 -0300

    barra lateral

--- paginaInicial.js
+++ paginaInicial.js
@@ -94,3 +94,25 @@ count = 1;
 }
 document.getElementById("radio"+count).checked = true;
 }
+
+var menuItem = document.querySelectorAll('.item-menu')
+
+function selectLink(){
+    menuItem.forEach((item)=>
+        item.classList.remove('ativo')
+    )
+    this.classList.add('ativo')
+}
+
+menuItem.forEach((item)=>
+    item.addEventListener('click', selectLink)
+)
+
+//expandir o menu
+
+var btnExp = document.querySelector('#btn-exp')
+var menuLateral = document.querySelector('.menu-lateral')
+
+btnExp.addEventListener('click', function(){
+    menuLateral.classList.toggle('expandir')
+})
